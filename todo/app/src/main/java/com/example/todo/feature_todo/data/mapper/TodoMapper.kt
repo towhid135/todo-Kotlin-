91ec7f8740efecdba9137ecd0c9ec54c81222f44
towhid135 <towhidulislam252@gmail.com>
2024-10-22 07:40:19 +0600

    mapper function created

---
+++ todo/app/src/main/java/com/example/todo/feature_todo/data/mapper/TodoMapper.kt
@@ -0,0 +1,134 @@
+package com.example.todo.feature_todo.data.mapper
+
+import com.example.todo.feature_todo.data.local.dto.LocalTodoItem
+import com.example.todo.feature_todo.data.remote.dto.RemoteTodoItem
+import com.example.todo.feature_todo.domain.model.TodoItem
+
+fun TodoItem.toLocalTodoItem():LocalTodoItem{
+    return LocalTodoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun TodoItem.toRemoteTodoItem():RemoteTodoItem{
+    return RemoteTodoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun LocalTodoItem.toTodoItem():TodoItem{
+    return TodoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun LocalTodoItem.toRemoteTodoItem():RemoteTodoItem{
+    return RemoteTodoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun RemoteTodoItem.toTodoItem():TodoItem{
+    return TodoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun RemoteTodoItem.toLocalTodoItem():LocalTodoItem{
+    return LocalTodoItem(
+        title = title,
+        description = description,
+        timestamp = timestamp,
+        completed = completed,
+        archived = archived,
+        id = id
+    )
+}
+
+fun List<TodoItem>.toRemoteTodoItemList():List<RemoteTodoItem>{
+    return this.map { todo -> RemoteTodoItem(
+        title = todo.title,
+        description = todo.description,
+        timestamp = todo.timestamp,
+        completed = todo.completed,
+        archived = todo.archived,
+        id = todo.id
+    ) }
+}
+
+fun List<LocalTodoItem>.toTodoItemListFromLocal():List<TodoItem>{
+    return this.map{
+        todo -> TodoItem(
+        title = todo.title,
+        description = todo.description,
+        timestamp = todo.timestamp,
+        completed = todo.completed,
+        archived = todo.archived,
+        id = todo.id
+        )
+    }
+}
+
+fun List<LocalTodoItem>.toRemoteTodoItemListFromLocal():List<RemoteTodoItem>{
+    return this.map{
+            todo -> RemoteTodoItem(
+        title = todo.title,
+        description = todo.description,
+        timestamp = todo.timestamp,
+        completed = todo.completed,
+        archived = todo.archived,
+        id = todo.id
+    )
+    }
+}
+
+fun List<RemoteTodoItem>.toTodoItemListFromRemote(): List<TodoItem>{
+    return this.map {todo ->
+        TodoItem(
+            title = todo.title,
+            description = todo.description,
+            timestamp = todo.timestamp,
+            completed = todo.completed,
+            archived = todo.archived,
+            id = todo.id
+        )
+    }
+}
+
+fun List<RemoteTodoItem>.toLocalTodoItemListFromRemote(): List<LocalTodoItem>{
+    return this.map {todo ->
+        LocalTodoItem(
+            title = todo.title,
+            description = todo.description,
+            timestamp = todo.timestamp,
+            completed = todo.completed,
+            archived = todo.archived,
+            id = todo.id
+        )
+    }
+}
---
+++ todo/app/src/main/java/com/example/todo/feature_todo/domain/model/TodoItem.kt
@@ -0,0 +1,10 @@
+package com.example.todo.feature_todo.domain.model
+
+data class TodoItem(
+    val title:String,
+    val  description:String,
+    val timestamp:Long,
+    val completed:Boolean,
+    val archived:Boolean,
+    val id:Int?
+)
