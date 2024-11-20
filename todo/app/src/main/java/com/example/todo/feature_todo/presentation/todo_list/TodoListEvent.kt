384141c202b196eb9b6c1bfb6378ff7cd7835d8b
towhid135 <towhidulislam252@gmail.com>
2024-11-20 07:39:52 +0600

    created TodoListEvent

--- todo/app/src/main/java/com/example/todo/feature_todo/presentation/todo_list/TodoListEvent.kt
+++ todo/app/src/main/java/com/example/todo/feature_todo/presentation/todo_list/TodoListEvent.kt
@@ -1,2 +1,12 @@
 package com.example.todo.feature_todo.presentation.todo_list
 
+import com.example.todo.feature_todo.domain.model.TodoItem
+import com.example.todo.feature_todo.domain.util.TodoItemOrder
+
+sealed class TodoListEvent {
+    data class Sort(val todoItemOrder: TodoItemOrder):TodoListEvent()
+    data class Delete(val todo:TodoItem):TodoListEvent()
+    data class ToggleCompleted(val todo:TodoItem):TodoListEvent()
+    data class ToggleArchived(val todo:TodoItem):TodoListEvent()
+    object UndoDelete:TodoListEvent()
+}
