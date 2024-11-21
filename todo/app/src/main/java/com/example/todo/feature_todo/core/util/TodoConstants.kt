63f92ff8112f1f02b53a8bc2a0a9936f03d6b853
towhid135 <towhidulislam252@gmail.com>
2024-11-21 07:37:11 +0600

    getTodoItems function added to the view model

--- todo/app/src/main/java/com/example/todo/feature_todo/core/util/TodoConstants.kt
+++ todo/app/src/main/java/com/example/todo/feature_todo/core/util/TodoConstants.kt
@@ -3,3 +3,7 @@ package com.example.todo.feature_todo.core.util
 object TodoConstants {
     const val EMPTY_TITLE_OR_DESCRIPTION = "Both the todo title and description must be populated."
 }
+
+object TodoListStrings {
+    const val CANT_GET_TODOS = "Error: Could not retrieve Todo Items"
+}
--- todo/app/src/main/java/com/example/todo/feature_todo/presentation/todo_list/TodoListViewModel.kt
+++ todo/app/src/main/java/com/example/todo/feature_todo/presentation/todo_list/TodoListViewModel.kt
@@ -2,23 +2,58 @@ package com.example.todo.feature_todo.presentation.todo_list
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.todo.feature_todo.data.di.IoDispatcher
 import com.example.todo.feature_todo.domain.model.TodoItem
+import com.example.todo.feature_todo.domain.use_case.TodoUseCaseResult
 import com.example.todo.feature_todo.domain.use_case.TodoUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TodoListViewModel @Inject constructor(
     private val todoUseCases:TodoUseCases,
     @IoDispatcher private val dispatcher:CoroutineDispatcher
-){
+):ViewModel() {
     private val _state = mutableStateOf(TodoListState())
     val state:State<TodoListState> = _state
 
     private var undoTodoItem:TodoItem? = null
     private var getTodoItemJob:Job? = null
+
+    private val errorHandler = CoroutineExceptionHandler{_,e ->
+        e.printStackTrace()
+        _state.value = _state.value.copy(error = e.message, isLoading = false)
+    }
+
+    fun getTodoItems(){
+        getTodoItemJob?.cancel()
+
+        getTodoItemJob = viewModelScope.launch(dispatcher+errorHandler){
+            val result = todoUseCases.getTodoItems(todoItemOrder = _state.value.todoItemOrder)
+
+            when(result){
+                is TodoUseCaseResult.Success -> {
+                    _state.value = _state.value.copy(
+                        todoItems = result.todoItems,
+                        todoItemOrder = _state.value.todoItemOrder,
+                        isLoading = false
+                    )
+                }
+                is TodoUseCaseResult.Error -> {
+                    _state.value = _state.value.copy(
+                        error = result.message,
+                        isLoading = false
+                    )
+                }
+            }
+
+        }
+    }
 
 }
