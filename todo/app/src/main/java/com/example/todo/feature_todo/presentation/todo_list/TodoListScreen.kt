aa9b52922378dedc63b64766fda58588ad7a4244
towhid135 <towhidulislam252@gmail.com>
2024-12-17 19:25:40 +0600

    modalnavigation drawer & drawer content created

--- todo/app/src/main/java/com/example/todo/feature_todo/presentation/todo_list/TodoListScreen.kt
+++ todo/app/src/main/java/com/example/todo/feature_todo/presentation/todo_list/TodoListScreen.kt
@@ -1,15 +1,29 @@
 package com.example.todo.feature_todo.presentation.todo_list
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.todo.R
+import com.example.todo.core.util.TodoListStrings
+import com.example.todo.feature_todo.domain.util.TodoItemOrder
+import com.example.todo.feature_todo.presentation.todo_list.components.DrawerOptions
 
 @Composable
 fun TodoListScreen(
@@ -28,5 +42,37 @@ fun TodoListScreen(
         R.drawable.background_portrait
     }else{
         R.drawable.background_landscape
+    }
+
+    LaunchedEffect(key1 = true) {
+        viewModel.getTodoItems()
+    }
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = { DrawerContent(state.todoItemOrder, onOrderChange = {order ->
+            viewModel.onEvent(TodoListEvent.Sort(order))
+        }) }
+    ) {
+        //
+    }
+}
+
+@Composable
+fun DrawerContent(todoItemOrder:TodoItemOrder,onOrderChange:(TodoItemOrder) -> Unit){
+    Box(modifier = Modifier.fillMaxWidth(0.65f)){
+        ModalDrawerSheet {
+            Text(
+                text = TodoListStrings.SORT_BY,
+                modifier = Modifier.padding(16.dp),
+                fontSize = 34.sp,
+                lineHeight = 18.sp
+            )
+            HorizontalDivider()
+            DrawerOptions(
+                todoItemOrder = todoItemOrder,
+                onOrderChange = onOrderChange
+            )
+        }
     }
 }
